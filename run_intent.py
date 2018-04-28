78826b079b607ac366909b38f2f2059f63843710
nani <nani76.gsrao@gmail.com>
2018-04-28 23:55:13 +0100

    IR changed

--- run_intent.py
+++ run_intent.py
@@ -1,5 +1,6 @@
 import ahlogger
-
+from services.rmBroadLinkService import RMBroadLInk
 
 if __name__ == "__main__":
-   ahlogger.log("this is a test")
+   broadlink = RMBroadLInk('192.168.0.17')
+   broadlink.toggle_power()
